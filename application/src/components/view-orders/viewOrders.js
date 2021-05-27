f0f111666ef3d16c51e2a89ad354c25bc4eef923
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-05-27 16:41:12 -0700

    feat(viewOrders): delete button functions

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,52 +1,81 @@
-import React, { Component } from 'react';
-import { Template } from '../../components';
-import { SERVER_IP } from '../../private';
-import './viewOrders.css';
+import React, { Component } from "react";
+import { Template } from "../../components";
+import { SERVER_IP } from "../../private";
+import "./viewOrders.css";
+
+const EDIT_ORDER_URL = `${SERVER_IP}/api/edit-order`;
+const DELETE_ORDER_URL = `${SERVER_IP}/api/delete-order`;
 
 class ViewOrders extends Component {
-    state = {
-        orders: []
-    }
+  state = {
+    orders: [],
+  };
 
-    componentDidMount() {
-        fetch(`${SERVER_IP}/api/current-orders`)
-            .then(response => response.json())
-            .then(response => {
-                if(response.success) {
-                    this.setState({ orders: response.orders });
-                } else {
-                    console.log('Error getting orders');
-                }
-            });
-    }
+  componentDidMount() {
+    fetch(`${SERVER_IP}/api/current-orders`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.success) {
+          this.setState({ orders: response.orders });
+        } else {
+          console.log("Error getting orders");
+        }
+      });
+  }
 
-    render() {
-        return (
-            <Template>
-                <div className="container-fluid">
-                    {this.state.orders.map(order => {
-                        const createdDate = new Date(order.createdAt);
-                        return (
-                            <div className="row view-order-container" key={order._id}>
-                                <div className="col-md-4 view-order-left-col p-3">
-                                    <h2>{order.order_item}</h2>
-                                    <p>Ordered by: {order.ordered_by || ''}</p>
-                                </div>
-                                <div className="col-md-4 d-flex view-order-middle-col">
-                                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
-                                    <p>Quantity: {order.quantity}</p>
-                                 </div>
-                                 <div className="col-md-4 view-order-right-col">
-                                     <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
-                                 </div>
-                            </div>
-                        );
-                    })}
+  deleteOrder(orderId) {
+    fetch(DELETE_ORDER_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        id: orderId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        let orders = this.state.orders.filter((order) => order._id !== orderId);
+        this.setState({ orders });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  render() {
+    return (
+      <Template>
+        <div className="container-fluid">
+          {this.state.orders.map((order) => {
+            const createdDate = new Date(order.createdAt);
+            return (
+              <div className="row view-order-container" key={order._id}>
+                <div className="col-md-4 view-order-left-col p-3">
+                  <h2>{order.order_item}</h2>
+                  <p>Ordered by: {order.ordered_by || ""}</p>
                 </div>
-            </Template>
-        );
-    }
+                <div className="col-md-4 d-flex view-order-middle-col">
+                  <p>
+                    Order placed at{" "}
+                    {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}
+                  </p>
+                  <p>Quantity: {order.quantity}</p>
+                </div>
+                <div className="col-md-4 view-order-right-col">
+                  <button className="btn btn-success">Edit</button>
+                  <button
+                    className="btn btn-danger"
+                    value={this.state.order_item}
+                    onClick={() => this.deleteOrder(order._id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Template>
+    );
+  }
 }
 
 export default ViewOrders;
