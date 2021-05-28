aaef33ff0be91c94945cee21ad0b3509f4e5c896
Brandon Johnson <brandonjohnson@Brandons-MacBook-Air.local>
2021-05-27 17:10:59 -0700

    feat(appRouter):adds route to single orderitem

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, Login, OrderForm, ViewOrders } from '../components';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Main, Login, OrderForm, ViewOrders } from "../components";
 
 const AppRouter = (props) => {
   return (
@@ -8,9 +8,10 @@ const AppRouter = (props) => {
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
       <Route path="/order" exact component={OrderForm} />
+      <Route path="/order/:id" component={OrderForm} />
       <Route path="/view-orders" exact component={ViewOrders} />
     </Router>
   );
-}
+};
 
 export default AppRouter;
